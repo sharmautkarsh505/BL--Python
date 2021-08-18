b1371895c383f63490077aa74440b83eea7b6b75
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-18 13:19:28 +0530

    [Utkarsh] feat: take_input forms list;concat_list forms string

---
+++ Basic Python DS/concat_list.py
@@ -0,0 +1,21 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 10:11:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to concatenate all elements in a list into a string and return it.
+"""
+
+def concat_list(input_list):
+    string=''
+    for element in input_list:
+        string+=element
+    return string
+
+
+def take_input():
+    input_string=[]
+    input_string=[ele for ele in input('Enter list elements: ').split(',')]
+    return input_string
+
+print(concat_list(take_input()))
