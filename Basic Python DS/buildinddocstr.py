32973ea7d43bba9408d9ae9705c4f3643a201a13
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-18 13:06:50 +0530

    [Utkarsh] feat: fn performs task, and displays docs

---
+++ Basic Python DS/buildinddocstr.py
@@ -0,0 +1,11 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 09:27:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to print the documents (syntax, description etc.) of Python built-in
+function(s).
+"""
+
+print(abs(-3))
+print(abs.__doc__)
