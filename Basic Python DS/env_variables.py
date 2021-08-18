af1d0714814456e476627e57cf09a5936de9da4e
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-18 22:36:26 +0530

    [Utkarsh] refactor:changes in header file

--- Basic Python DS/env_variables.py
+++ Basic Python DS/env_variables.py
@@ -1,8 +1,8 @@
 """
 Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
 Date: 2021-08-11 13:26:00
-Last Modified by:---
-Last Modified time:----
+Last Modified by:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Last Modified time:2021-08-18 22:34:00
 Title : Write a python program to access environment variables.
 """
 
