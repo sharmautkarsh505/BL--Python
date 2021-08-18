de2d5bbe71d4b1cbf3559d803ddcdff969294b4e
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-18 15:16:26 +0530

    [Utkarsh] feat: sys module;getrecursionlimit method

---
+++ Basic Python DS/recursion_limit.py
@@ -0,0 +1,10 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 12:18:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to get the current value of the recursion limit.
+"""
+
+import sys 
+print(sys.getrecursionlimit())
