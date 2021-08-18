0bb8aadb31b575b8a80ee258ac64c3c1328b44ec
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-18 14:44:41 +0530

    getpass module,getuser method gives username

---
+++ Basic Python DS/get_username.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 11:20:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to get the current username
+"""
+
+import getpass
+
+#getuser() returns login name of user
+print(getpass.getuser())
