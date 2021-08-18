49135273b1ffac80efe14e1835abf9fa5ed3d2a2
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-18 20:28:09 +0530

    [Utkarsh] feat: os. path module;exists method to check

---
+++ Basic Python DS/fileexists.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 10:45:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to check whether a file exists.
+"""
+import os
+print(os.path.exists('cdr.py'))
+
+
+
