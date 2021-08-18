7538dc3cd87332153b1451acd184c3db60fab050
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-18 13:03:57 +0530

    [Utkarsh] feat: display 1st and last item of list

---
+++ Basic Python DS/selclist.py
@@ -0,0 +1,10 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 09:25:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to display the first and last colors from the following list.
+"""
+
+color_list = ["Red","Green","White" ,"Black"]
+print(color_list[0],color_list[-1])
