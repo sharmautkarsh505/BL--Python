506772df20ea516340922c806a388c6c8a4538b1
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-18 16:08:45 +0530

    [Utkarsh] refactor:added missing heading

--- Basic Python DS/Listntuple.py
+++ Basic Python DS/Listntuple.py
@@ -1,3 +1,11 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 14:02:00
+Last Modified by:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Last Modified time:2021-08-18 16:03:00
+Title : Write a Python program to get numbers divisible by fifteen from a list using an anonymous function.
+"""
+
 #could the user input any number of numbers
 #do not need to change the input to integer as in the sample list all elements are strings 
 input_string=input('Enter numbers seperated by comma:')
