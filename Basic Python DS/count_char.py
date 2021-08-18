bb3b81796b57be4cabf4f3ca78e0f94374aa7b19
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-18 16:02:19 +0530

    count occur of specific char

---
+++ Basic Python DS/count_char.py
@@ -0,0 +1,10 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 12:30:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to count the number occurrence of a specific character in a string.
+"""
+my_string='Hello python world'
+
+print(my_string.count('l'))
