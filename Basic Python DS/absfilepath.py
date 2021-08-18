b91fc1b9672a4a42b1e2142af0ddd33541f6b120
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-18 14:53:30 +0530

    os.path module; abspath method

---
+++ Basic Python DS/absfilepath.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 11:30:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to get an absolute file path.
+"""
+
+import os
+
+#returns absolute path of the file, what does absolute mean?
+print(os.path.abspath('week1.pdf'))
