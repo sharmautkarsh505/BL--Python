0de2782385fd093548dea71042bc3d82b193a485
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-18 15:05:08 +0530

    sys module,builtin_module_names method

---
+++ Basic Python DS/avlblbuiltimods.py
@@ -0,0 +1,11 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 12:07:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to find the available built-in modules.
+"""
+
+import sys
+
+print(sorted(sys.builtin_module_names))
