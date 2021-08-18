351ae39e23b6814f6ed5db506678b24753b25b1d
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-18 16:00:10 +0530

    [Utkarsh] feat: lambda fn to check divisibility

---
+++ Basic Python DS/lmbdadivisible.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 2:02:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to get numbers divisible by fifteen from a list using an anonymous function.
+"""
+
+number_list=[122,225,75,625,90]
+
+divisible_list=list(filter(lambda x: x%15==0,number_list))
+print(divisible_list)
