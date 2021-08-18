25d2bbe6241450fad3cbb39332edf796536d3781
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-18 15:30:56 +0530

    [Utkarsh] feat: os module,system method to clr screen

---
+++ Basic Python DS/clrscreen.py
@@ -0,0 +1,10 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 12:40:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to clear the screen or terminal.
+"""
+import os 
+
+os.system('cls')
