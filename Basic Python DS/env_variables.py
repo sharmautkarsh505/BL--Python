01b877f8a51114f0ad2492a74044f8b28e7787d6
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-18 14:42:27 +0530

    os.environ to access env variables

---
+++ Basic Python DS/env_variables.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-07 20:00:00
+Last Modified by:---
+Last Modified time:----
+Title : Find the factors of the given number
+"""
+
+import os 
+#mapping object representing the string environment,key value pairs
+print(os.environ)
+#print(os.environ['HOME'])
