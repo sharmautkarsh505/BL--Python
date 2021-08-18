80e5e7284c5c2ab1e544860fa92692cf79c3c866
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-18 15:22:45 +0530

    [Utkarsh] feat: sys time using datetime module

---
+++ Basic Python DS/systime.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 12:36:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to get the system time.
+"""
+
+import datetime
+
+print('System Time')
+print(datetime.datetime.now())
