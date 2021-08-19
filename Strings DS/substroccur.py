622605c494121e302c58623385963b13096dca17
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 14:11:31 +0530

    [Utkarsh] feat: count a substring in a string

---
+++ Strings DS/substroccur.py
@@ -0,0 +1,10 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-13 13:17:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to count occurrences of a substring in a string.
+"""
+player_name='Kyrie Kyrie Irving'
+
+print(player_name.count('Kyrie'))
