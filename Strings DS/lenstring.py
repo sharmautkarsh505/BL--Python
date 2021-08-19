0426d2d50b4204a73418148ae3775683d8436d55
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 13:55:25 +0530

    [Utkarsh] feat: calc len of string

---
+++ Strings DS/lenstring.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-13 11:25:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python program to calculate the length of a string.
+"""
+
+player_name='Kyrie Irving'
+
+#whitespace will also account for one character
+print(len(player_name))
