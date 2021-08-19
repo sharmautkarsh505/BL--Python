b509640bd72d8fd9bae856eb6d1031da30b20dab
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 09:54:47 +0530

    [Utkarsh] feat: format func to convert int to binary

---
+++ Basic Python DS/inttobinary.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 15:27:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to convert an integer to binary keep leading zeros
+"""
+
+x=5
+y=8
+print(format(x,"04b"))
+print(format(y,'05b'))
