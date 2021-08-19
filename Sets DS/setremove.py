ddd2b895dadf092adea314cae866ac8df57aa9d2
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 12:16:40 +0530

    [Utkarsh] feat: remove elements from set

---
+++ Sets DS/setremove.py
@@ -0,0 +1,11 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-12 13:04:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python program to remove item(s) from set
+"""
+basketball_players={'Kyrie Irving', 'John Wall', 'Jayson Tatum', 'Lebron James'}
+basketball_players.remove('Jayson Tatum')
+
+print(basketball_players)
