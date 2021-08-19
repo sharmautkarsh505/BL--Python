e75c01a1952b6781ae4700224384d6f582b8c624
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 14:12:53 +0530

    [Utkarsh] feat: reverse string

---
+++ Strings DS/revstring.py
@@ -0,0 +1,10 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-13 13:23:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to reverse a string.
+"""
+fav_fruit='jackfruit'
+
+print(fav_fruit[::-1])
