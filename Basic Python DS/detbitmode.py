146582b85ae1e61c3fa95ac778dd9eaa65582dfd
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 10:23:18 +0530

    [Utkarsh] refactor: changes in the header file

--- Basic Python DS/detbitmode.py
+++ Basic Python DS/detbitmode.py
@@ -1,6 +1,6 @@
 """
 Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
-Date: 2021-08-11 12:30:00
+Date: 2021-08-11 15:44:00
 Last Modified by:---
 Last Modified time:----
 Title : Write a Python program to determine if the python shell is executing in 32bit or 64bit mode
