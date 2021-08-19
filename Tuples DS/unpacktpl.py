75f2db4b9a538bd5b8e1bf1a619180d4f2d7f5d7
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 13:18:15 +0530

    [Utkarsh] feat: unpack elements in tuple to variables

---
+++ Tuples DS/unpacktpl.py
@@ -0,0 +1,11 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-13 09:39:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to unpack a tuple in several variables.
+"""
+numbers=(1,3,5)
+
+n1,n2,n3=numbers
+print(n1,n2,n3)
