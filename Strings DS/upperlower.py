3f5ea015286e967cf8380f724db99c9c770acc00
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 14:03:39 +0530

    [Utkarsh] feat: covert user input to upper and lowercase

---
+++ Strings DS/upperlower.py
@@ -0,0 +1,11 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-13 12:15:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python script that takes input from the user and displays that input back in upper and lower cases.
+"""
+fav_car=input('Enter your favourite car:')
+
+print(fav_car.upper())
+print(fav_car.lower())
