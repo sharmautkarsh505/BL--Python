07279dd1570784fc28363aa762cacc0161118151
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 12:46:49 +0530

    [Utkarsh] feat: find smallest element in list

---
+++ Lists DS/smallestnlist.py
@@ -0,0 +1,10 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-12 16:28:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to get the smallest number from a list.
+"""
+num_list=list(range(1,11))
+
+print(min(num_list))
