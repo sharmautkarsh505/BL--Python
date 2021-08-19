6f27c1c0f3f09ca80f6b5c2582275ea24df8644c
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 11:24:54 +0530

    [Utkarsh] feat: add key to dict

---
+++ Dictionary DS/addindict.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-12 09:40:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python script to add a key to a dictionary.
+"""
+sample_dict={'Damian Lillard': 1928, 'Stephen Curry': 2015}
+
+sample_dict['Julius Randle']=1712
+
+print(sample_dict)
