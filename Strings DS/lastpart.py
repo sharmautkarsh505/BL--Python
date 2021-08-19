b423fd03bc4db5c9fdd030937a1a94973638221b
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 14:06:50 +0530

    [Utkarsh] feat: get last part of dtring before some char

---
+++ Strings DS/lastpart.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-13 12:41:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python program to get the last part of a string before a specified character.
+"""
+url_link1="https://www.w3resource.com/python-exercises"
+url_link2="https://www.w3resource.com/python"
+
+print(url_link1.rsplit('/')[-1])
+print(url_link2.rsplit('/')[-1])
