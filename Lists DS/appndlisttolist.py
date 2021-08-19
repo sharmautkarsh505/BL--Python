05711e207dd52bdcc11faebe975817306f75beb4
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 13:02:53 +0530

    [Utkarsh] feat: appends one list to another

---
+++ Lists DS/appndlisttolist.py
@@ -0,0 +1,13 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-12 18:32:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python program to append a list to the second list.
+"""
+num_list1=list(range(1,11))
+num_list2=list(range(11,21))
+
+num_list3=num_list1+num_list2
+print(num_list3)
+
