b9aaf197c0e7f8b993f7daf4f56e57f9016e6d65
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 12:32:55 +0530

    [Utkarsh] feat: make frozen set

---
+++ Sets DS/frozenset.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-07 15:07:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to use of frozensets.
+"""
+vowels={'a','e','i','o','u'}
+
+#vowel is now an immutable object
+vowels=frozenset(vowels)
+
