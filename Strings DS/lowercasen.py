19d55f6f4ac2ec7036f6d73fdfd808f2cb54a959
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 14:15:30 +0530

    [Utkarsh] feat: lowercase n chars in string

---
+++ Strings DS/lowercasen.py
@@ -0,0 +1,13 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-13 13:33:00
+Last Modified by:---
+Last Modified time:----
+Title :Write a Python program to lowercase first n characters in a string.
+"""
+
+player_name='KYRIE IRVING'
+
+print(player_name[:3:].lower()+player_name[3::])
+
+
