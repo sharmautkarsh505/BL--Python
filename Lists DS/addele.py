ea1b60a752ecae1d5b213e90f336156c14bf47ff
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 12:42:19 +0530

    [Utkarsh] feat: add elements of list

---
+++ Lists DS/addele.py
@@ -0,0 +1,9 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-12 16:07:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to sum all the items in a list.
+"""
+num_list=list(range(1,11))
+print(sum(num_list))
