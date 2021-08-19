71fde430f3d70c45dbde97d2a63ad116db3fd07d
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 12:55:43 +0530

    [Utkarsh] feat: clone list

---
+++ Lists DS/cpylist.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-12 17:11:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to clone or copy a list.
+"""
+num_list=list(range(1,11))
+
+copied_list=num_list.copy()
+
+print(copied_list)
