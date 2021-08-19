bfc4eb4c1de7760b265fc0805e57cffc3001d64d
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 13:28:05 +0530

    [Utkarsh] feat: change list to tuple

---
+++ Tuples DS/list2tuple.py
@@ -0,0 +1,11 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-13 10:32:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to convert a list to a tuple.
+"""
+arbitrary_tuple=(1,122,11,4,5,90,5,6,8,7,5)
+
+arbitrary_list=list(arbitrary_tuple)
+print(arbitrary_list)
