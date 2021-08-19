2cbc9d0574de318079922b3c5e10497c33fcf3d6
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 12:10:04 +0530

    [Utkarsh] feat: create set

---
+++ Sets DS/createset.py
@@ -0,0 +1,10 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-12 12:40:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to create a set.
+"""
+basketball_players={'Lebron James','Kyrie Irving','John Wall'}
+print(basketball_players)
+
