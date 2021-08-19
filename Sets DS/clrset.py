5c52455814c7e926ed93c5622ce2bf8ee0a32b2f
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 12:29:22 +0530

    [Utkarsh] feat: clear the set

---
+++ Sets DS/clrset.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-12 14:00:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python program to clear a set.
+"""
+basketball_players={'Lebron James','Kyrie Irving','John Wall'}
+
+basketball_players.clear()
+
+print(basketball_players)
