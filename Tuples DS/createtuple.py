a69249888b4e0bbc35614a25c344120f560c8441
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 13:12:38 +0530

    [Utkarsh] feat: create tuple

---
+++ Tuples DS/createtuple.py
@@ -0,0 +1,15 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-13 09:20:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python program to create a tuple.
+"""
+
+#Creating a tuple for dimensions
+dimensions=(200,300)
+print(dimensions)
+
+#dimensions[0]=100 should not work, but the entire tuple can be changed
+dimensions=(300,400)
+print(dimensions)
