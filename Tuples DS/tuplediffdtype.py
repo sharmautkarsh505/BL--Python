e516fbfaf52b4d6cf68a91bf9b892193f10cf5f2
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 13:15:56 +0530

    [Utkarsh] feat: tuple with diff dtypes

---
+++ Tuples DS/tuplediffdtype.py
@@ -0,0 +1,10 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-13 09:27:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python program to create a tuple with different data types.
+"""
+#name of player and ppg
+player_stats=('Stephen Curry',32.8)
+print(player_stats)
