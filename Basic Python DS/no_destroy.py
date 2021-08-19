787bce40583179331d2505a0ec6f660ca4bcaa21
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 09:19:54 +0530

    [Utkarsh] feat: empty list/dict/tuple/ini no to zero

---
+++ Basic Python DS/no_destroy.py
@@ -0,0 +1,11 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 13:13:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to empty a variable without destroying it.
+"""
+player_stats_dictionary={'Lebron James':16.2,'Stephen Curry':32.8}
+num_list=list(range(2,12,2))
+print(type(player_stats_dictionary)())
+print(type(num_list)())
