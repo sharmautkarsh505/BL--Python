d019fbc90cf64cc2b6724e662687ac2126b1a2f0
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 12:14:35 +0530

    [Utkarsh] feat: add elements to set

---
+++ Sets DS/setadd.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-12 12:57:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to add member(s) in a set.
+"""
+basketball_players={'Lebron James','Kyrie Irving','John Wall'}
+
+basketball_players.add('Jayson Tatum')
+
+print(basketball_players)
