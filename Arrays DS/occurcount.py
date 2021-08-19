de345fcd7d3895e1a423be49bc8435e8f44a9a43
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 11:07:40 +0530

    [Utkarsh] feat: count occurences in array

---
+++ Arrays DS/occurcount.py
@@ -0,0 +1,13 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 16:32:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python program to get the number of occurrences of a specified element in an
+array.
+"""
+import array
+
+arr5=array.array('i',[1,2,2,3,4,5,2])
+
+print(arr5.count(2))
