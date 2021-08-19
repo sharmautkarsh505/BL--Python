31a416daad8602e96a679f151b5971d684961ab3
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 11:37:37 +0530

    [Utkarsh] feat: remove key form dict

---
+++ Dictionary DS/rmvkey.py
@@ -0,0 +1,17 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-12 10:30:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python program to remove a key from a dictionary.
+"""
+dict={'Damian Lillard':1928,
+      'Stephen Curry':2015,
+      'Julius Randle':1712,
+      'Bradley Beal':1878,
+      'Kyrie Irving':1451, 
+      'Jayson Tatum':1692}
+
+dict.pop('Jayson Tatum')
+
+print(dict)
