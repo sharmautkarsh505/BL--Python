f7de7d60e72f64e113b7e640425eae7792a1a0fa
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 13:31:54 +0530

    [Utkarsh] feat: slice tuple

---
+++ Tuples DS/slicetuple.py
@@ -0,0 +1,13 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-13 11:02:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python program to slice a tuple.
+"""
+
+arbitrary_tuple=(1,122,11,4,5,90,5,6,8,7,5)
+
+arbitrary_tuple=arbitrary_tuple[1:5]
+print(arbitrary_tuple)
+
