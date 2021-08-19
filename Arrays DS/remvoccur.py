757f21db25df62d0ff09bd2bbb6bb5a00b106739
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 11:08:39 +0530

    [Utkarsh] feat: remove first occurence from array

---
+++ Arrays DS/remvoccur.py
@@ -0,0 +1,13 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 17:12:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python program to remove the first occurrence of a specified element from an array.
+"""
+import array
+
+arr5=array.array('i',[1,2,2,3,4,5,2])
+
+arr5.remove(2)
+print(arr5)
