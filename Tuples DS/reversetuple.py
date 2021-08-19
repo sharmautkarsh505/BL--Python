9dedf1685b961529af98b7f2e74335c9be15f611
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 13:33:45 +0530

    [Utkarsh] feat: reverse tuple

---
+++ Tuples DS/reversetuple.py
@@ -0,0 +1,11 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-13 11:13:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to reverse a tuple.
+"""
+arbitrary_tuple=(1,122,11,4,5,90,5,6,8,7,5)
+
+arbitrary_tuple[::-1]
+print(arbitrary_tuple)
