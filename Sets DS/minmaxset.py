54da0e873b9c202ac37cfe7012648ed31ec7f8ec
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 12:36:02 +0530

    [Utkarsh] feat: min and max of a set

---
+++ Sets DS/minmaxset.py
@@ -0,0 +1,14 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-12 15:21:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to find maximum and the minimum value in a set.
+"""
+ppg_players={32.8,26.5,18.4,29.6}
+
+maximum_ppg=max(ppg_players)
+minimum_ppg=min(ppg_players)
+print(maximum_ppg)
+print(minimum_ppg)
+
