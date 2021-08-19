ad21c0514c842166222de7a078475beb864f7d87
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 10:35:20 +0530

    [Utkarsh] feat: reverse array

---
+++ Arrays DS/revarray.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 16:30:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python program to reverse the order of the items in the array.
+"""
+import array
+
+arr5=array.array('i',[1,2,3,4,5])
+arr5.reverse()
+print(arr5)
