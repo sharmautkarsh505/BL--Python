bee68595952180123c17657348d8bddb99254c71
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 12:45:00 +0530

    [Utkarsh] feat: mutiply elements of list

---
+++ Lists DS/multiplylst.py
@@ -0,0 +1,13 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-12 16:12:00
+Last Modified by:----
+Last Modified time:----
+Title :Write a Python program to multiplies all the items in a list.
+"""
+num_list=list(range(1,11))
+product=1
+for number in num_list:
+    product=number*product
+
+print(product)
