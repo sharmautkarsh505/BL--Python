269871254d06ecdd69672cb8030ddcd60a3d51cd
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 12:12:56 +0530

    [Utkarsh] feat: iterate thr set

---
+++ Sets DS/setiter.py
@@ -0,0 +1,10 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-12 12:45:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python program to iteration over sets.
+"""
+basketball_players={'Lebron James','Kyrie Irving','John Wall'}
+
+[print(basketball_player) for basketball_player in basketball_players]
