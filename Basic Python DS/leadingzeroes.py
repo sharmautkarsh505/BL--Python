c415077272236cd26147883965ef758aa2f01e15
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 09:27:41 +0530

    feat: use zfill method to add leading zeroes to strings

---
+++ Basic Python DS/leadingzeroes.py
@@ -0,0 +1,11 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 14:20:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to add leading zeroes to a string.
+"""
+
+numerical_string='1234'
+numerical_string_wzeroes=numerical_string.zfill(6)
+print(numerical_string_wzeroes)
