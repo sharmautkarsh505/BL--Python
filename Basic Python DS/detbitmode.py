92222ce39cef162e050570c07f8f873d3714d05a
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-19 09:59:54 +0530

    [Utkarsh] feat: check bit mode

---
+++ Basic Python DS/detbitmode.py
@@ -0,0 +1,11 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-11 12:30:00
+Last Modified by:---
+Last Modified time:----
+Title : Write a Python program to determine if the python shell is executing in 32bit or 64bit mode
+on operating system.
+"""
+
+import struct
+print(struct.calcsize("P") * 8)
