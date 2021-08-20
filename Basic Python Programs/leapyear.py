96664842f5e5c0bfd38b5a8e28b557791a24ed7b
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-20 15:07:16 +0530

    [Utkarsh] refactor:Added try-except for exceptions

--- Basic Python Programs/leapyear.py
+++ Basic Python Programs/leapyear.py
@@ -1,17 +1,28 @@
 """
 Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
 Date: 2021-08-07 18:30:00
-Last Modified by:----
-Last Modified time:----
+Last Modified by:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Last Modified time:2021-08-20 15:06:00
 Title : Check if input year is a leap year or not
 """
- #Enter the year
-year=int(input('Enter the year:'))
+class RangeError(Exception):
+    pass
+try:
+    #Enter the year
+    year=int(input('Enter the year:'))
 
-#For years not turning the century,check divisibilty of year by 4 and 100
-#For years at the turn of the century, check divisibility by 400
-if (year%4==0 and year%100!=0 and len(str(year))==4) or (year%400==0 and len(str(year))==4):
-    print(f'{year} is a leap year')
-elif (year%4!=0 and len(str(year))==4) or (year%100==0 and year%400!=0 and len(str(year))==4):
-    print(f'{year} is not a leap year')
-    
+    if len(str(year))<4 or len(str(year))>4:
+        raise RangeError
+
+except ValueError:
+    print('Please enter a year that has a postive integer value')
+except RangeError:
+    print('Please ensure that the input year is a 4 digit integer value')
+
+else:
+    #For years not turning the century,check divisibilty of year by 4 and 100
+    #For years at the turn of the century, check divisibility by 400
+    if (year%4==0 and year%100!=0) or year%400==0:
+        print(f'{year} is a leap year')
+    elif year%4!=0 or (year%100==0 and year%400!=0):
+        print(f'{year} is not a leap year')
