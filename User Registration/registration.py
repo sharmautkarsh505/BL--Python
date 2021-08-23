38b631f2209cd5831a281e49e96a0474b297c3f7
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-23 09:21:12 +0530

    [Utkarsh] feat:corrected all logic as per requirement

--- User Registration/registration.py
+++ User Registration/registration.py
@@ -1,43 +1,40 @@
 import re
 
 def first_name(fname):
-    if fname.isalpha()==True and len(fname)>=3 and fname[0]==fname[0].upper() and fname[1:len(fname)-1]==fname[1:len(fname)-1].lower():
-        return fname
+    pattern_of_fname=re.compile(r"^[A-Z][a-z]{2,}$")
+    if pattern_of_fname.fullmatch(fname):
+        return True 
     else:
-        pass 
+        return False
 
 def last_name(lname):
-    if lname.isalpha()==True and len(lname)>=3 and lname[0]==lname[0].upper() and lname[1:len(lname)-1]==lname[1:len(lname)-1].lower():
-        return lname
+    pattern_of_lname=re.compile(r"^[A-Z][a-z]{2,}$")
+    if pattern_of_lname.fullmatch(lname):
+        return True
     else:
-        pass
+        return False
 
 
 def email_id(eid):
-    pattern_of_email_1=re.compile(r"[a-z]{3}[a-z]*\@[a-z]{2}[a-z]*\.co")
-    pattern_of_email_2=re.compile(r"[a-z]{3}[a-z]+\.[a-z]{3}[a-z]+\@[a-z]{2}[a-z]+\.co\.in")
+    pattern_of_email_1=re.compile(r"^[a-z]{3,}\@bl\.co$")
+    pattern_of_email_2=re.compile(r"^[a-z]{3,}\.[a-z]{3,}\@bl\.co\.in$")
 
-    if pattern_of_email_1.fullmatch(eid)!=None or pattern_of_email_2.fullmatch(eid)!=None:
-        return eid
+    if pattern_of_email_1.fullmatch(eid) or pattern_of_email_2.fullmatch(eid):
+        return True
     else:
-        print('Invalid input')
+        return False
 
 def mobile_number(mno):
-    pattern_of_mobile_no=re.compile(r"\+?\d{2}\s\d{10}")
-    #match=pattern_of_mobile_no.fullmatch(mno)
-    valid_input=bool
-    if pattern_of_mobile_no.fullmatch(mno)!=None:
-        valid_input==True
+    pattern_of_mobile_no=re.compile(r"^\+?[1-9][0-9]\s[0-9]{10}$")
+    if pattern_of_mobile_no.fullmatch(mno):
+        return True
     else:
-        valid_input==False
-    return valid_input
-
+        return False
+    
 def user_password(password):
-    pattern_of_password=re.compile(r"^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")
+    pattern_of_password=re.compile(r"^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8}$")
 
     if pattern_of_password.fullmatch(password):
-        return password
+        return True
     else:
-        pass
-
-print(mobile_number('91 9923645994'))
+        return False
