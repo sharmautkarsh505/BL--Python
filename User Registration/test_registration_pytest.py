48c965012353c866db43ef11b0db0cbf70a88beb
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-23 11:00:53 +0530

    [Utkarsh] feat: pytest for user reg

--- User Registration/test_registration_pytest.py
+++ User Registration/test_registration_pytest.py
@@ -1,22 +1,27 @@
 import pytest
-import re
-
-def first_name(fname):
-    if fname.isalpha()==True and len(fname)>=3 and fname[0]==fname[0].upper() and fname[1:len(fname)-1]==fname[1:len(fname)-1].lower():
-        return fname
-    else:
-        return None
+from registration import first_name,last_name,email_id,mobile_number,user_password
 
 def test_fname():
-    pattern_fname=re.compile(r'[A-Z][a-z]{2,}')
-    assert re.fullmatch(pattern_fname,first_name('Utkarsh'))
-    assert re.fullmatch(pattern_fname,first_name('utkarsh'))
-    assert re.fullmatch(pattern_fname,first_name('L1james'))
-    
-    
+    assert first_name('Utkarsh')==True
+    assert first_name('UTkarsh')==False
+    assert first_name('utkarsh')==False
 
-def last_name(lname):
-    if lname.isalpha()==True and len(lname)>=3 and lname[0]==lname[0].upper() and lname[1:len(lname)-1]==lname[1:len(lname)-1].lower():
-        return lname
-    else:
-        pass
+def test_lname():
+    assert last_name('Sharma')==True
+    assert last_name('SHarma')==False
+    assert last_name('SHARMA')==False 
+
+def test_email():
+    assert email_id('utkarsh@bl.co')==True
+    assert email_id('utkarsh.sharma@bl.co.in')==True
+    assert email_id('UTKARSH@bl.co')==False 
+
+def test_mobile_number():
+    assert mobile_number('00 0000000000')==False
+    assert mobile_number('81 7989886451')==True
+    assert mobile_number('80 1234567092')==True
+
+def test_password():
+    assert user_password('U@1karsh')==True 
+    assert user_password('UTKARS1$')==True
+    assert user_password('&$Utkars')==False
