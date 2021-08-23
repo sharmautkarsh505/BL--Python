582045672cc5edfad213df80d4e586e1a9435805
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-23 09:24:33 +0530

    [Utkarsh] feat: used assertequal;imported logic

--- User Registration/test_registration_unittest.py
+++ User Registration/test_registration_unittest.py
@@ -1,22 +1,42 @@
 import unittest 
+from registration import first_name,last_name,email_id,mobile_number,user_password
 
 class UserValidation(unittest.TestCase):
 
     def test_fname(self):
-        self.assertRegex('Utkarsh',r'^[A-Z][a-z]{2,}$')
-        self.assertNotRegex('SAYMA',r'^[A-Z][a-z]{2,}$')
-        self.assertNotRegex('A1ana',r'^[A-Z][a-z]{2,}$')
-        self.assertRegex('Karan',r'^[A-Z][a-z]{2,}$')
-        self.assertNotRegex('Ka',r'^[A-Z][a-z]{2,}$')
+        self.assertEqual(first_name('Utkarsh'),True)
+        self.assertEqual(first_name('UTkarsh'),False)
+        self.assertEqual(first_name('utkarsh'),False)
+        self.assertEqual(first_name('Ka'),False)
+        self.assertEqual(first_name('K1aran'),False)
+        self.assertEqual(first_name('Utkarsh   '),False)
+
     def test_lname(self):
-        self.assertRegex('Sharma',r'^[A-Z][a-z]{2,}$')
+        self.assertEqual(last_name('Sharma'),True)
+        self.assertEqual(last_name('SHarma'),False)
+        self.assertEqual(last_name('Sh'),False)
+        self.assertEqual(last_name('S1har'),False)
+        self.assertEqual(last_name('SHARMA'),False)
+    
     def test_emailid(self):
-        self.assertRegex('utkarsh@bl.co',r"[a-z]{3,}\@bl\.co" or r"[a-z]{3,}+\.[a-z]{3,}\@bl\.co\.in")
-        self.assertRegex('utkarsh.sharma@bl.co.in',r"[a-z]{3,}\@bl\.co" or r"[a-z]{3,}+\.[a-z]{3,}\@bl\.co\.in")
-        self.assertRegex('utkarsh@bl.co',r"[a-z]{3,}\@bl\.co" or r"[a-z]{3,}+\.[a-z]{3,}\@bl\.co\.in")
-    def test_mobileno(self):
-        self.assertRegex('91 9923645994',r"\+?\d{2}\s\d{10}")  
-        self.assertRegex('+94 8823467891',r"\+?\d{2}\s\d{10}")
+        self.assertEqual(email_id('utkarsh@bl.co'),True)
+        self.assertEqual(email_id('utkarsh.sharma@bl.co.in'),True)
+        self.assertEqual(email_id('UTKARSH@bl.Co'),False)
+        self.assertEqual(email_id('uk.lm@bl.co.in'),False)
+        self.assertEqual(email_id('uke@bl.co'),True,)
+
+    def test_mobile_number(self):
+        self.assertEqual(mobile_number('00 0000000000'),False)
+        self.assertEqual(mobile_number('81 7989886451'),True)
+        self.assertEqual(mobile_number('80 1234567092'),True)
+        
+    
+    def test_password(self):
+        self.assertEqual(user_password('U@1karsh'),True)
+        self.assertEqual(user_password('UTKARS1$'),True)
+        self.assertEqual(user_password('&$Utkars'),False)
+        self.assertEqual(user_password('Ut@k1arsh'),False)
+        self.assertEqual(user_password('UK123@kl'),True)
 
 if __name__ == '__main__':
     unittest.main()
