228035016ae4094b1d3a3352ccd8eaf0f4cf1770
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-24 11:26:10 +0530

    [Utkarsh] feat: scalar multiplication for rectangular matrices and use of list comprehensions included

--- Matrix ops/scalarmul.py
+++ Matrix ops/scalarmul.py
@@ -5,6 +5,8 @@ Last Modified by:----
 Last Modified time:----
 Title : Scalar multiplication
 """
+#logic gets much clear once used for rectangular matrix
+#for square matrix
 A=[[1,1],[1,1]]
 
 for i in range(len(A)):
@@ -12,3 +14,19 @@ for i in range(len(A)):
         A[i][j]=2*A[i][j]
 
 print(A)
+
+#using list comprehension
+s_result=[[A[i][j]*4 for j in range(len(A[0]))] for i in range(len(A))]
+print(s_result)
+
+#for rectangular matrix 2x4
+B=[[1,2,3,4],[5,6,7,8]]
+
+for i in range(len(B)):
+    for j in range(len(B[0])):
+        B[i][j]=6*B[i][j]
+print(B)
+
+#using list comprehension
+r_result=[[B[i][j]*6 for j in range(len(B[0]))] for i in range(len(B))]
+print(B)
