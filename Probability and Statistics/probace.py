d46e799e4db899dfdf12e95eef26d283b082dc8e
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-27 16:30:33 +0530

    [Utkarsh] feat: prob drawing ace

---
+++ Probability and Statistics/probace.py
@@ -0,0 +1,15 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-26 10:19:00
+Last Modified by:----
+Last Modified time:----
+Title : Find probability of drawing an ace from pack of cards
+"""
+
+cards_in_deck=52
+
+aces_in_deck=4
+
+
+probability_aces=aces_in_deck/cards_in_deck
+print(probability_aces)
