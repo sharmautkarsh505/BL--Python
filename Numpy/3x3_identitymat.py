e730a74959896709458095af383d954a5c305315
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-31 16:06:38 +0530

    [Utkarsh] feat: created 3x3 identity matrix

---
+++ Numpy/3x3_identitymat.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-31 16:03:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python program to create a 3-D array with ones on a diagonal and zeros
+elsewhere.
+"""
+import numpy as np
+
+identity_matrix_3x3=np.identity(3)
+print(identity_matrix_3x3)
