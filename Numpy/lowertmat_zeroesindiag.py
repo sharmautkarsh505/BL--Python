463d09f9511d238c310aaa37c3441e2b9350e77c
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-31 16:18:57 +0530

    [Utkarsh] refactor: minor changes to code

--- Numpy/lowertmat_zeroesindiag.py
+++ Numpy/lowertmat_zeroesindiag.py
@@ -10,3 +10,8 @@ import numpy as np
 lower_triangular_matrix=np.tril(np.ones((3,3)),k=0)
 np.fill_diagonal(lower_triangular_matrix,val=0)
 print(lower_triangular_matrix)
+
+l_matrix=np.tril(np.ones((4,3)))
+print(l_matrix)
+np.fill_diagonal(l_matrix,val=0)
+print(l_matrix)
