0d23012a3aa5821b80f64a92581dc532285ba377
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-31 23:20:33 +0530

    [Utkarsh] feat: summary of data

---
+++ Pandas/dataframe_summary.py
@@ -0,0 +1,20 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-31 23:11:00
+Last Modified by:----
+Last Modified time:----
+Title :Write a Python program to display a summary of the basic information about a
+specified Data Frame and its data.
+"""
+import numpy as np
+import pandas as pd
+
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael',
+'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+data=pd.DataFrame(exam_data,index=labels)
+print(data.dtypes)
+print(data.describe())
