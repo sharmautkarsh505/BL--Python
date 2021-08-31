d5f402201f15318f6e3096f3c4ee1b719041996e
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-31 16:15:34 +0530

    [Utkarsh] feat: created matrix were elements on lower side of diagonals are 1, rest 0

---
+++ Numpy/lowertmat_zeroesindiag.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-31 16:08:00
+Last Modified by:----
+Last Modified time:----
+Title :Write a Python program to create an array which looks like below array.
+"""
+import numpy as np
+
+lower_triangular_matrix=np.tril(np.ones((3,3)),k=0)
+np.fill_diagonal(lower_triangular_matrix,val=0)
+print(lower_triangular_matrix)
