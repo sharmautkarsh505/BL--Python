46d9668c887252643ff7486994dd65bd0821efda
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-31 18:14:32 +0530

    [Utkarsh] refactor: change to header file

--- Numpy/supp_scientificn.py
+++ Numpy/supp_scientificn.py
@@ -1,8 +1,8 @@
 """
 Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
 Date: 2021-08-31 17:37:00
-Last Modified by:----
-Last Modified time:----
+Last Modified by:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Last Modified time:2021-08-31 17:56:00
 Title :Write a Python program to suppresses the use of scientific notation for small
 numbers in numpy array.
 """
