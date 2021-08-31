596475a8c0dff7a5ae5d8f24afd7e6473e27e64f
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-31 10:03:12 +0530

    [Utkarsh] feat: list to numpy array

---
+++ Numpy/list_to_nparr.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-31 09:59:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python program to convert a list of numeric value into a one-dimensional
+NumPy array.
+"""
+import numpy as np
+
+num_list=list(range(1,11))
+num_arr=np.array(num_list)
