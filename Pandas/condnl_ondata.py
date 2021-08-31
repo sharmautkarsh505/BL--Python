76a0e3d1275d893504196557f868c9e6f4f5f3b5
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-09-01 00:00:20 +0530

    [Utkarsh] feat: selc rows on condnl basis

---
+++ Pandas/condnl_ondata.py
@@ -0,0 +1,20 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-31 23:54:00
+Last Modified by:----
+Last Modified time:----
+Title :Write a Python program to select the rows where the number of attempts in the
+examination is greater than 2.
+"""
+import numpy as np
+import pandas as pd
+
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael',
+'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+data=pd.DataFrame(exam_data,index=labels)
+req_data=data[data['attempts']>=2]
+print(req_data)
