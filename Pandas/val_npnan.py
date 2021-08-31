0ab7379de72efd1b58006feeeb6ec997d01af454
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-09-01 00:48:30 +0530

    [Utkarsh] refactor: rectified date in header file

--- Pandas/val_npnan.py
+++ Pandas/val_npnan.py
@@ -1,6 +1,6 @@
 """
 Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
-Date: 2021-08-01 00:05:00
+Date: 2021-09-01 00:05:00
 Last Modified by:----
 Last Modified time:----
 Title :Write a Python program to select the rows where the score is missing, i.e. is NaN.
