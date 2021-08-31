fd9032b9bd9cfeb5d846b8fac64774dfa74f5270
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-31 10:10:27 +0530

    [Utkarsh] feat: created 3x3 matrix using numpy

---
+++ Numpy/np3x3.py
@@ -0,0 +1,12 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-31 10:06:00
+Last Modified by:----
+Last Modified time:----
+Title : Create a 3x3 matrix with values ranging from 2 to 10.
+"""
+import numpy as np
+
+
+matrix_3x3=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(matrix_3x3)
