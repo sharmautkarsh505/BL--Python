bba972dcbbb842ff3afa5384096d4aed2a39cf58
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-09-01 00:45:38 +0530

    [Utkarsh] feat: change score in row d

---
+++ Pandas/chng_score.py
@@ -0,0 +1,19 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-09-01 00:34:00
+Last Modified by:----
+Last Modified time:----
+Title :Write a Python program to change the score in row 'd' to 11.5.
+"""
+import numpy as np
+import pandas as pd
+
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael',
+'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+data=pd.DataFrame(exam_data,index=labels)
+data['score']['d']=11.5
+print(data)
