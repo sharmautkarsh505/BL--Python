8312ea30ca7cff36504077aff1080839c79d2ae6
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-31 10:15:41 +0530

    [Utkarsh] feat: zero vector with 10 vals;updtd 6th val

---
+++ Numpy/np_zero_up6.py
@@ -0,0 +1,11 @@
+""""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-31 10:11:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python program to create a null vector of size 10 and update sixth value to 11.
+"""
+import numpy as np 
+
+zero_matrix=np.zeros(10)
+zero_matrix[6]=11
