9a0956c8617e7004d6975940e929432319d394fd
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-31 15:37:54 +0530

    [Utkarsh] feat: compare two numpy arrays

---
+++ Numpy/compare.py
@@ -0,0 +1,23 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-31 15:33:00
+Last Modified by:----
+Last Modified time:----
+Title : Write a Python program compare two arrays using numpy.
+"""
+import numpy as np 
+
+arr1=np.array([1,2])
+arr2=np.array([4,5])
+
+
+print(np.greater(arr1,arr2))
+print(np.less(arr1,arr2))
+print(np.greater_equal(arr2,arr1))
+print(np.less_equal(arr2,arr1))
+
+
+
+
+#Array a: [1 2]
+#Array b: [4 5]
