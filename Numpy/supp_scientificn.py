a20488d0c55fc52307a95071ac0500b8f61a0d17
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-08-31 17:41:14 +0530

    [Utkarsh] feat: suppress use of scientific notation

---
+++ Numpy/supp_scientificn.py
@@ -0,0 +1,13 @@
+"""
+Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
+Date: 2021-08-31 17:37:00
+Last Modified by:----
+Last Modified time:----
+Title :Write a Python program to suppresses the use of scientific notation for small
+numbers in numpy array.
+"""
+import numpy as np
+np.set_printoptions(suppress=True)
+my_arr=np.array([1.60000000e-10,1.60000000e+00,1.20000000e+03,2.35000000e-01])
+
+print(my_arr)
