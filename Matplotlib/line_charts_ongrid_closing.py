3790b807e8795aa8861523c70ac4e01744c1a45c
sharmautkarsh505 <64436705+sharmautkarsh505@users.noreply.github.com>
2021-09-01 15:19:31 +0530

    [Utkarsh] refactor: minor change in header file

--- Matplotlib/line_charts_ongrid_closing.py
+++ Matplotlib/line_charts_ongrid_closing.py
@@ -1,8 +1,8 @@
 """
 Author:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
 Date: 2021-09-01 14:14:00
-Last Modified by:Utkarsh Sharma(sharmautkarsh2396@gmail.com)
-Last Modified time:2021-09-01 12:57:00
+Last Modified by:----
+Last Modified time:----
 Title : Write a Python program to display the grid and draw line charts of the closing value of
 Alphabet Inc. between October 3, 2016 to October 7, 2016. Customized the grid lines with
 linestyle -, width .5. and color blue.
